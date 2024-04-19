c9a6b85df23d7bf3881cfed81d9e6d1522852760
マイケル <74095454+ProWebEngineer@users.noreply.github.com>
2024-04-19 17:46:18 +0900

    Create fetch_videos.py

---
+++ fetch_videos.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+api_key = os.getenv('YOUTUBE_API_KEY')
+channel_id = 'YOUR_CHANNEL_ID'
+url = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&channelId={channel_id}&part=snippet&order=date&maxResults=5"
+
+response = requests.get(url)
+videos = response.json()
+
+with open('latest-videos.md', 'w') as file:
+    for video in videos['items']:
+        video_id = video['id']['videoId']
+        title = video['snippet']['title']
+        thumbnail_url = video['snippet']['thumbnails']['medium']['url']
+        file.write(f"![{title}]({thumbnail_url})\n")
+        file.write(f"[{title}](https://www.youtube.com/watch?v={video_id})\n\n")
