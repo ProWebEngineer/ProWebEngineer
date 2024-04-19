825355ebcd6ea10ac77775fc32c5529e4912a8af
マイケル <74095454+ProWebEngineer@users.noreply.github.com>
2024-04-19 19:27:58 +0900

    Update fetch_videos.py

--- fetch_videos.py
+++ fetch_videos.py
@@ -4,18 +4,23 @@ import os
 # 環境変数からAPIキーを取得
 api_key = os.getenv('YOUTUBE_API_KEY')
 channel_id = 'UCGq2Y94R8nXkJw0m2YFO4fA'
-url = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&channelId={channel_id}&part=snippet&order=date&maxResults=5"
+url = f"https://www.googleapis.com/youtube/v3/search?key={api_key}&channelId={channel_id}&part=snippet&order=date&maxResults=3"
 
 response = requests.get(url)
 videos = response.json()
-print('videos', videos)  # デバッグ用の動画データ出力
 
+# Markdownファイルに書き込むHTMLコードを生成
+html_content = '<p>\n'
+for video in videos['items']:
+    video_id = video['id']['videoId']
+    title = video['snippet']['title']
+    thumbnail_url = video['snippet']['thumbnails']['medium']['url']
+    video_link = f"https://www.youtube.com/watch?v={video_id}"
+    html_content += f'  <a href="{video_link}">\n'
+    html_content += f'    <img src="{thumbnail_url}" alt="{title}" style="width: 160px; height: auto; margin-right: 10px;">\n'
+    html_content += '  </a>\n'
+html_content += '</p>\n'
+
+# HTMLコンテンツをファイルに書き込み
 with open('latest-videos.md', 'w') as file:
-    for video in videos['items']:
-        video_id = video['id']['videoId']
-        title = video['snippet']['title']
-        thumbnail_url = video['snippet']['thumbnails']['medium']['url']
-        # リンク付きの画像をMarkdown形式で書き込み
-        link = f"https://www.youtube.com/watch?v={video_id}"
-        markdown_line = f"[![{title}]({thumbnail_url})]({link})\n\n"
-        file.write(markdown_line)
+    file.write(html_content)
